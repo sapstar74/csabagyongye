fde7da211c31b3e411a2ca55258f050119ae829c
Zsiga Gábor Csaba <zsigagabor@Zsigas-MacBook-Pro.local>
2025-08-29 16:06:44 +0200

    Update question for P. Mobil - 47_P_Mobil

--- topics/magyar_zenekarok_uj.py
+++ topics/magyar_zenekarok_uj.py
@@ -1,5 +1,5 @@
 # Auto-generated questions file
-# Generated on: 2025-08-29 16:01:05
+# Generated on: 2025-08-29 16:06:44
 
 QUESTIONS = [
     {
@@ -228,7 +228,7 @@ QUESTIONS = [
         "question": "Ki az előadó?",
         "options": [
             "P. Mobil",
-            "Bonanza Banzai",
+            "P.Box",
             "Republic",
             "Omega",
         ],
