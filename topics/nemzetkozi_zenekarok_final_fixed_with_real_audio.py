8ee6f87ddde7f9a8b35d137831978163b1a51445
Zsiga Gábor Csaba <zsigagabor@Zsigas-MacBook-Pro.local>
2025-09-07 12:36:51 +0200

    Update question for Alphaville - Stakka Bo - Here We Go

--- topics/nemzetkozi_zenekarok_final_fixed_with_real_audio.py
+++ topics/nemzetkozi_zenekarok_final_fixed_with_real_audio.py
@@ -1,5 +1,5 @@
 # Auto-generated questions file
-# Generated on: 2025-09-07 12:33:31
+# Generated on: 2025-09-07 12:36:51
 
 QUESTIONS = [
     {
@@ -915,7 +915,7 @@ QUESTIONS = [
     {
         "question": "Ki az előadó?",
         "options": [
-            "Alphaville",
+            "Stakka Bo",
             "Ultravox",
             "New Order",
             "A-ha",
