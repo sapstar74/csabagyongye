6d6a7ff96ec4e9a436c1bf6db8b87f136080c8a0
Zsiga Gábor Csaba <zsigagabor@Zsigas-MacBook-Pro.local>
2026-02-03 12:38:40 +0100

    Bulk add tracks (sorozat_focimek)

--- topics/sorozat_focimek.py
+++ topics/sorozat_focimek.py
@@ -1,5 +1,5 @@
 # Auto-generated questions file
-# Generated on: 2026-02-03 12:35:01
+# Generated on: 2026-02-03 12:38:38
 
 QUESTIONS = [
     {
@@ -27,6 +27,118 @@ QUESTIONS = [
         "explanation": "Stranger Things - Title Sequence | Netflix",
         "audio_file": "02. Stranger Things - Stranger Things.mp3",
         "song_title": "Stranger Things",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "Friends",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "Friends - 'I'll Be There For You'",
+        "audio_file": "s2TyVQGoCYo.mp3",
+        "song_title": "'I'll Be There For You'",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "The Office",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "The Office - Intro",
+        "audio_file": "4iisysmwB_k.mp3",
+        "song_title": "Intro",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "AnissaRaquel",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "AnissaRaquel - The Fresh Prince Of Bel Air Theme Song",
+        "audio_file": "1nCqRmx3Dnw.mp3",
+        "song_title": "The Fresh Prince Of Bel Air Theme Song",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "Dave Porter - Topic",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "Dave Porter - Topic - Breaking Bad Main Title Theme",
+        "audio_file": "ilfYnhXD-bE.mp3",
+        "song_title": "Breaking Bad Main Title Theme",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "Pratik Vaidya",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "Pratik Vaidya - Better Call Saul Theme by Little Barrie Full Original Song",
+        "audio_file": "Unknown_Artist_Unknown_Title.mp3",
+        "song_title": "Better Call Saul Theme by Little Barrie Full Original Song",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "Emily Hinnekens",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "Emily Hinnekens - The X-Files Opening Credits",
+        "audio_file": "v4-GcS1UQyg.mp3",
+        "song_title": "The X-Files Opening Credits",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "kasper933",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "kasper933 - Twin Peaks Intro",
+        "audio_file": "yFMaEIHIrGw.mp3",
+        "song_title": "Twin Peaks Intro",
+        "topic": "sorozat_focimek",
+    },
+    {
+        "question": "Melyik sorozat főcímdala ez?",
+        "options": [
+            "The Sopranos Opening Credits Theme Song",
+            "Game of Thrones",
+            "Stranger Things",
+            "Szerkeszthető opció",
+        ],
+        "correct": 0,
+        "explanation": "The Sopranos Opening Credits Theme Song - The Sopranos | HBO",
+        "audio_file": "mJpNmYeooQE.mp3",
+        "song_title": "The Sopranos | HBO",
         "topic": "sorozat_focimek",
     },
 ]
